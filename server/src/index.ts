db5d9027aa3267e744b9302e7f284e6cfa307a33
Amitrajit007 <amitrajitsarkar4@gmail.com>
2025-11-13 13:13:02 +0530

    chore: idk what i just did . sorry.

--- server/src/index.ts
+++ server/src/index.ts
@@ -7,6 +7,12 @@ app.get("/", (req: Request, res: Response) => {
   res.json({ msg: `Hello World from ${PORT}` });
 });
 
+app.get("/ihateexams", (req: Request, res: Response) => {
+  res.json({
+    msg: "why there are 1000s of exams in a single a AY......😡😡😡😡😡😡😡😡",
+  });
+});
+
 app.listen(PORT, () => {
   console.log(`Listening at ${PORT}`);
 });
