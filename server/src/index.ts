30ca9d46d3c34012b61079783d51a5eaf525f24e
Amitrajit007 <amitrajitsarkar4@gmail.com>
2025-12-05 00:30:19 +0530

    chore: added the cors and the websocket room.

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,18 +1,30 @@
-import express, { Request, Response } from "express";
 import "dotenv/config";
+
+import express, { Request, Response } from "express";
+import { Server, Socket } from "socket.io";
+import { createServer } from "node:http";
+import cors from "cors";
+
 const PORT = process.env.PORT || 5000;
+
 const app = express();
+const server = createServer(app);
+const io = new Server(server);
+
 app.use(express.json());
+app.use(cors);
+
 app.get("/", (req: Request, res: Response) => {
   res.json({ msg: `Hello World from ${PORT}` });
 });
 
-app.get("/ihateexams", (req: Request, res: Response) => {
-  res.json({
-    msg: "why there are 1000s of exams in a single a AY......😡😡😡😡😡😡😡😡",
-  });
+app.get("/health", (req: Request, res: Response) => {
+  res.send("OK");
 });
 
-app.listen(PORT, () => {
+io.on("connection", (socket: Socket) => {
+  console.log("a user connected");
+});
+server.listen(PORT, () => {
   console.log(`Listening at ${PORT}`);
 });
