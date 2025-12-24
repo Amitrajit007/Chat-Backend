7c2b2edc8a79878f65dfe8d71d45f366d169a93d
Amitrajit007 <amitrajitsarkar4@gmail.com>
2025-12-25 03:37:07 +0530

    feat: add dummy client to validate Socket.IO handshake

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,21 +1,25 @@
 import "dotenv/config";
-
+// imports
 import express, { Request, Response } from "express";
 import { Server, Socket } from "socket.io";
 import { createServer } from "node:http";
 import cors from "cors";
 
+// ENVs
 const PORT = process.env.PORT || 5000;
 
-const app = express();
-const server = createServer(app);
-const io = new Server(server);
+// declarations------
+const app = express(); //request handler
+const server = createServer(app); //real HTTP server
+const io = new Server(server); //WebSocket layer attached to that server
 
+// middlewares
 app.use(express.json());
-app.use(cors);
+app.use(cors());
 
+// basic routes for sanity now
 app.get("/", (req: Request, res: Response) => {
-  res.json({ msg: `Hello World from ${PORT}` });
+  res.json({ msg: `Active @ ${PORT}` });
 });
 
 app.get("/health", (req: Request, res: Response) => {
@@ -23,8 +27,20 @@ app.get("/health", (req: Request, res: Response) => {
 });
 
 io.on("connection", (socket: Socket) => {
-  console.log("a user connected");
+  console.log("a user connected: ", socket.id);
+
+  socket.on("chat-message", (text: string) => {
+    console.log("message recieved: ", text);
+
+    //sending the msg to all others
+    socket.broadcast.emit("chat-message", text);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("Client disconnect with id: ", socket.id);
+  });
 });
+
 server.listen(PORT, () => {
   console.log(`Listening at ${PORT}`);
 });
